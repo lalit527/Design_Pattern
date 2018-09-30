041bb905307ce569e32d1dc31c55ed067b090bf0
lalit527 <lalit.slg007@gmail.com>
2018-09-30 13:26:42 +0530

    Observer pattern in java

---
+++ Startegy/python/strategy.py
@@ -0,0 +1,31 @@
+import types
+class Strategy:
+  
+  def __init__(self, function = None):
+    self.name = "Default Strategy"
+    if function:
+      self.execute = types.MethodType(function, self)
+
+  def execute(self):
+    print("{}! is used!".format(self.name))
+
+
+def strategy_one(self):
+  print("{}! is used to execute method 1".format(self.name))
+
+
+def strategy_two(self):
+  print("{}! is used to execute method 2".format(self.name))
+
+
+if __name__ == "__main__":
+  s0 = Strategy()
+  s0.execute()
+
+  s1 = Strategy(strategy_one)
+  s1.name = "Strategy One"
+  s1.execute()
+
+  s2 = Strategy(strategy_two)
+  s2.name = "Strategy Two"
+  s2.execute()
