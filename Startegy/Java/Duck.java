de59c7b544fea806d49539a3606dfebb60a9017e
lalit527 <lalit.slg007@gmail.com>
2018-09-30 12:09:32 +0530

    Added strategy pattern in java

---
+++ Startegy/Java/Duck.java
@@ -0,0 +1,29 @@
+public abstract class Duck {
+  FlyBehaviour flyBehaviour;
+  QuackBehaviour quackBehaviour;
+  public Duck() {
+
+  }
+
+  public abstract void display();
+
+  public void performFly() {
+    flyBehaviour.fly();
+  }
+
+  public void performQuack() {
+    quackBehaviour.quack();
+  }
+
+  public void swim() {
+    System.out.println("All ducks float, even decoys");
+  }
+
+  public void setFlyBehaviour(FlyBehaviour fb) {
+    this.flyBehaviour = fb;
+  }
+
+  public void setQuackBehaviour(QuackBehaviour qb) {
+    this.quackBehaviour = qb;
+  }
+}
